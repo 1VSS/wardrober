99f352027745ec2284e0f15818cab0f2ac63dcaf
1vss <vstringari20@gmail.com>
2024-07-21 17:06:28 -0300

    initial commit

---
+++ src/test/java/com/vss/wardrober/WardroberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vss.wardrober;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WardroberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
